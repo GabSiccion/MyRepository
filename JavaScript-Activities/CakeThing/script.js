c13439cfd27c42b2ac8f1601067f1ab1859f1325
GabSiccion <gabsiccion@outlook.com>
2023-01-15 23:18:33 +0800

    header nav logo icon

--- JavaScript-Activities/CakeThing/script.js
+++ JavaScript-Activities/CakeThing/script.js
@@ -6,10 +6,31 @@ function shapechange(element) {
 }
 
 function icingcolorchange(element) {
-    document.getElementById("cake").style.backgroundColor = element.value;
+  document.getElementById("cake").style.backgroundColor = element.value;
 }
 
 function textchange(element) {
-    console.log(element.value);
-    document.getElementById("cake-text").innerHTML = element.value;
+  console.log(element.value);
+  document.getElementById("cake-text").innerHTML = element.value;
+}
+
+function sizechange(selectedSize) {
+  let cake = document.getElementById("cake");
+
+  switch (selectedSize) {
+    case "Small":
+      cake.style.height = "300px";
+      cake.style.width = "300px";
+      break;
+    case "Medium":
+      cake.style.height = "450px";
+      cake.style.width = "450px";
+      break;
+    case "Large":
+      cake.style.height = "600px";
+      cake.style.width = "600px";
+      break;
+    default:
+      console.log("broken");
+  }
 }
